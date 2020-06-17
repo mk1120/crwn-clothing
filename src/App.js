f297539c3ba886548f7e95c42b5eb7fea19b4d2a
mk1120 <kitmk1120@gmail.com>
2020-06-18 07:57:53 +0800

    collection done

--- src/App.js
+++ src/App.js
@@ -3,23 +3,14 @@ import "./App.css";
 import { Route, Switch } from "react-router-dom";
 
 import HomePage from "./pages/homepage/homepage.component";
-
-const Hats = (props) => {
-  console.log(props);
-  return (
-    <div>
-      <h1>HATS PAGE {props.match.params.hatsId}</h1>
-      <button onClick={() => props.history.push("/")}>click</button>
-    </div>
-  );
-};
+import ShopPage from "./pages/shop/shop.component";
 
 const App = () => {
   return (
     <div>
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats/:hatsId" component={Hats} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
